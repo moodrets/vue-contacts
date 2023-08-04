085e4065ad48e13fa731dc3146b9a419f11ab382
Rets Dima <retsdmitriy@gmail.com>
2023-08-04 18:02:24 +0500

    dist to gitignore

--- dist/assets/ContactsEdit-33d8083f.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./Form.vue_vue_type_style_index_0_lang-fd0be32a.js";import{d as t,o as e,q as a}from"./index-540386ec.js";import"./useContacts-5585ce08.js";const p=t({__name:"ContactsEdit",setup(r){return(n,s)=>(e(),a(o))}});export{p as default};
